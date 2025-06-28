56c791788f51d8777ca22905fce31c925665ae7c
Socrat Mohammad <127431597+socratmhamad@users.noreply.github.com>
2025-06-28 08:00:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("6767447234:AAHODYTwpqlNl0mbeGLK9qAtgKVHfHC0e40")
 DOWNLOAD_FOLDER = 'downloads'
 
 if not os.path.exists(DOWNLOAD_FOLDER):
